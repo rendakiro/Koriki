f9390da2cc99ea97a80cd8719685b47a6088d7d6
Alfonso <s_suneku@hotmail.com>
2024-06-19 00:01:58 +0200

    Añado Menu para Satellaview y Sufami turbo

--- base/.simplemenu/scripts/consoles.sh
+++ base/.simplemenu/scripts/consoles.sh
@@ -2,22 +2,32 @@ consoles="/mnt/SDCARD/.simplemenu/section_groups/consoles.ini"
 
 cat > ${consoles} <<EOF
 [CONSOLES]
-consoleList = NES,FDS,SNES,MSU-1,SGB,VIRTUAL BOY,SEGA SG-1000,MASTER SYSTEM,SEGA GENESIS,MSU-MD,SEGA CD,SEGA 32X,ATARI 2600,ATARI 5200,ATARI 7800,INTELLIVISION,NEO GEO CD,PC ENGINE,PC ENGINE CD,TURBOGRAFX-CD,PLAYSTATION,ODYSSEY2
+consoleList = NES,FDS,SNES,SATELLAVIEW,SUFAMI TURBO,MSU-1,SGB,VIRTUAL BOY,SEGA SG-1000,MASTER SYSTEM,SEGA GENESIS,MSU-MD,SEGA CD,SEGA 32X,ATARI 2600,ATARI 5200,ATARI 7800,INTELLIVISION,NEO GEO CD,PC ENGINE,PC ENGINE CD,PC-FX,PLAYSTATION,ODYSSEY2
 
 [NES]
-execs = /mnt/SDCARD/.simplemenu/launchers/fceumm_libretro,/mnt/SDCARD/.simplemenu/launchers/nestopia_libretro,/mnt/SDCARD/.simplemenu/launchers/nestopia_miyoo_libretro
+execs = /mnt/SDCARD/.simplemenu/launchers/fceumm_libretro
 romDirs = /mnt/SDCARD/Roms/nes/
 romExts = .nes,.zip,.7z
 
 [FDS]
-execs = /mnt/SDCARD/.simplemenu/launchers/fceumm_libretro,/mnt/SDCARD/.simplemenu/launchers/nestopia_libretro,/mnt/SDCARD/.simplemenu/launchers/nestopia_miyoo_libretro
+execs = /mnt/SDCARD/.simplemenu/launchers/fceumm_libretro
 romDirs = /mnt/SDCARD/Roms/fds/
 romExts = .fds,.zip,.7z
 
 [SNES]
-execs = /mnt/SDCARD/.simplemenu/launchers/mednafen_supafaust_libretro,/mnt/SDCARD/.simplemenu/launchers/mednafen_supafaust_plus_libretro,/mnt/SDCARD/.simplemenu/launchers/snes9x_libretro,/mnt/SDCARD/.simplemenu/launchers/snes9x2005_plus_libretro,/mnt/SDCARD/.simplemenu/launchers/snes9x2005_libretro,/mnt/SDCARD/.simplemenu/launchers/snes9x2002_libretro,/mnt/SDCARD/.simplemenu/launchers/snes9x2010_libretro,/mnt/SDCARD/.simplemenu/launchers/snes9x_next_libretro
+execs = /mnt/SDCARD/.simplemenu/launchers/snes9x_libretro,/mnt/SDCARD/.simplemenu/launchers/mednafen_supafaust_libretro,/mnt/SDCARD/.simplemenu/launchers/mednafen_supafaust_plus_libretro,/mnt/SDCARD/.simplemenu/launchers/snes9x2005_plus_libretro,/mnt/SDCARD/.simplemenu/launchers/snes9x2005_libretro,/mnt/SDCARD/.simplemenu/launchers/snes9x2002_libretro,/mnt/SDCARD/.simplemenu/launchers/snes9x2010_libretro,/mnt/SDCARD/.simplemenu/launchers/snes9x_next_libretro
 romDirs = /mnt/SDCARD/Roms/snes/
 romExts = .smc,.sfc,.zip,.7z
+
+[SATELLAVIEW]
+execs = /mnt/SDCARD/.simplemenu/launchers/snes9x_libretro
+romDirs = /mnt/SDCARD/Roms/satellaview/
+romExts = .bs,.zip,.7z
+
+[SUFAMI TURBO]
+execs = /mnt/SDCARD/.simplemenu/launchers/snes9x_libretro
+romDirs = /mnt/SDCARD/Roms/sufami/
+romExts = .st,.zip,.7z
 
 [MSU-1]
 execs = /mnt/SDCARD/.simplemenu/launchers/msu1_libretro
@@ -26,7 +36,7 @@ romExts = .smc,.sfc,.zip,.7z
 aliasFile = /mnt/SDCARD/.simplemenu/alias_MSU-1.txt
 
 [SGB]
-execs = /mnt/SDCARD/.simplemenu/launchers/msgb_libretro,/mnt/SDCARD/.simplemenu/launchers/tgbdual_libretro
+execs = /mnt/SDCARD/.simplemenu/launchers/msgb_libretro
 romDirs = /mnt/SDCARD/Roms/sgb/
 romExts = .gb,.gbc,.zip,.7z
 
@@ -36,17 +46,17 @@ romDirs = /mnt/SDCARD/Roms/virtualboy/
 romExts = .vb,.vboy,.bin,.zip,.7z
 
 [SEGA SG-1000]
-execs = /mnt/SDCARD/.simplemenu/launchers/gearsystem_libretro,/mnt/SDCARD/.simplemenu/launchers/genesis_plus_gx_libretro,/mnt/SDCARD/.simplemenu/launchers/picodrive_standalone
+execs = /mnt/SDCARD/.simplemenu/launchers/genesis_plus_gx_libretro
 romDirs = /mnt/SDCARD/Roms/sg-1000/
 romExts = .zip,.sg,.7z
 
 [MASTER SYSTEM]
-execs = /mnt/SDCARD/.simplemenu/launchers/picodrive_libretro,/mnt/SDCARD/.simplemenu/launchers/genesis_plus_gx_libretro,/mnt/SDCARD/.simplemenu/launchers/gearsystem_libretro,/mnt/SDCARD/.simplemenu/launchers/picodrive_standalone
+execs = /mnt/SDCARD/.simplemenu/launchers/genesis_plus_gx_libretro
 romDirs = /mnt/SDCARD/Roms/mastersystem/
 romExts = .zip,.sms,.7z
 
 [SEGA GENESIS]
-execs = /mnt/SDCARD/.simplemenu/launchers/genesis_plus_gx_libretro,/mnt/SDCARD/.simplemenu/launchers/picodrive_libretro,/mnt/SDCARD/.simplemenu/launchers/picodrive_standalone
+execs = /mnt/SDCARD/.simplemenu/launchers/genesis_plus_gx_libretro
 romDirs = /mnt/SDCARD/Roms/megadrive/
 romExts = .zip,.bin,.smd,.md,.mdx,.gen,.7z
 
@@ -57,12 +67,12 @@ romExts = .md
 aliasFile = /mnt/SDCARD/.simplemenu/alias_MSU-MD.txt
 
 [SEGA CD]
-execs = /mnt/SDCARD/.simplemenu/launchers/genesis_plus_gx_libretro,/mnt/SDCARD/.simplemenu/launchers/picodrive_standalone, /mnt/SDCARD/.simplemenu/launchers/picodrive_libretro
+execs = /mnt/SDCARD/.simplemenu/launchers/genesis_plus_gx_libretro
 romDirs = /mnt/SDCARD/Roms/segacd/
 romExts = .chd,.cue
 
 [SEGA 32X]
-execs = /mnt/SDCARD/.simplemenu/launchers/picodrive_libretro,/mnt/SDCARD/.simplemenu/launchers/picodrive_standalone
+execs = /mnt/SDCARD/.simplemenu/launchers/picodrive_libretro
 romDirs = /mnt/SDCARD/Roms/sega32x/
 romExts = .zip,.32x,.7z
 
@@ -72,7 +82,7 @@ romDirs = /mnt/SDCARD/Roms/atari2600/
 romExts = .bin,.a26,.zip,.7z
 
 [ATARI 5200]
-execs = /mnt/SDCARD/.simplemenu/launchers/a5200_libretro,/mnt/SDCARD/.simplemenu/launchers/atari800_libretro
+execs = /mnt/SDCARD/.simplemenu/launchers/atari800_libretro
 romDirs = /mnt/SDCARD/Roms/atari5200/
 romExts = .bin,.a52,.zip,.7z
 
@@ -101,13 +111,13 @@ execs = /mnt/SDCARD/.simplemenu/launchers/mednafen_pce_fast_libretro,/mnt/SDCARD
 romDirs = /mnt/SDCARD/Roms/pcenginecd/
 romExts = .pce,.tg16,.cue,.chd,.zip,.7z
 
-[TURBOGRAFX-CD]
+[PC-FX]
 execs = /mnt/SDCARD/.simplemenu/launchers/mednafen_pcfx_libretro
 romDirs = /mnt/SDCARD/Roms/pcfx/
 romExts = .cue,.chd,.zip,.7z
 
 [PLAYSTATION]
-execs = /mnt/SDCARD/.simplemenu/launchers/pcsx_rearmed_miyoo_libretro,/mnt/SDCARD/.simplemenu/launchers/pcsx_rearmed_libretro,/mnt/SDCARD/.simplemenu/launchers/pcsx_rearmed_libretro_old,/mnt/SDCARD/.simplemenu/launchers/pcsx_rearmed_libretro_shaun
+execs = /mnt/SDCARD/.simplemenu/launchers/pcsx_rearmed_miyoo_libretro,/mnt/SDCARD/.simplemenu/launchers/pcsx_rearmed_libretro
 romDirs = /mnt/SDCARD/Roms/psx/
 romExts = .pbp,.chd,.zip,.cue,.img,.iso,.m3u,.mdf
 
