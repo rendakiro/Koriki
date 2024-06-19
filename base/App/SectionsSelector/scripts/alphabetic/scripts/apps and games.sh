0293a1f6bb7cabca14b0c737baedd1ffb5297168
Alfonso <s_suneku@hotmail.com>
2024-06-19 09:30:36 +0200

    Cambios en la APP "SectionsSelector" para mi modelo de carpetas

--- base/App/SectionsSelector/scripts/alphabetic/scripts/apps and games.sh
+++ base/App/SectionsSelector/scripts/alphabetic/scripts/apps and games.sh
@@ -2,18 +2,7 @@ consoles="/mnt/SDCARD/.simplemenu/section_groups/apps and games.ini"
 
 cat > ${consoles} <<EOF
 [CONSOLES]
-consoleList = AMIGA,AMSTRAD CPC,APPS,ARDUINO,ATARI 2600,ATARI 5200,ATARI 7800,ATARI LYNX,ATARI ST,COMMODORE 64,CPS1,CPS2,CPS3,DAPHNE,DOOM,DOS,FDS,FINALBURN ALPHA,FINALBURN NEO,GAME & WATCH,GAME BOY,GAME BOY ADVANCE,GAME BOY COLOR,GAME GEAR,GAMES,INTELLIVISION,MAME,MASTER SYSTEM,MSU-1,MSU-MD,MSX,NES,NEO GEO,NEO GEO CD,NEO GEO POCKET,NINTENDO DS,ODYSSEY2,OPENBOR,OVERLAYS,PC98,PC ENGINE,PC ENGINE CD,PICO-8,PLAYSTATION,POKEMON MINI,QUAKE,SCUMMVM,SGB,SEGA 32X,SEGA CD,SEGA GENESIS,SEGA SG-1000,SNES,SUPERVISION,TIC-80,VIRTUAL BOY,WOLF3D,WONDERSWAN,X68000,ZX SPECTRUM
-
-
-[AMIGA]
-execs = /mnt/SDCARD/.simplemenu/launchers/puae_libretro,/mnt/SDCARD/.simplemenu/launchers/uae4arm_libretro,/mnt/SDCARD/.simplemenu/launchers/puae2021_libretro
-romDirs = /mnt/SDCARD/Roms/AMIGA/
-romExts = .adf,.adz,.dms,.fdi,.ipf,.hdf,.hdz,.lha,.slave,.info,.cue,.ccd,.nrg,.mds,.iso,.chd,.uae,.m3u,.zip,.7z,.rp9
-
-[AMSTRAD CPC]
-execs = /mnt/SDCARD/.simplemenu/launchers/crocods_libretro
-romDirs = /mnt/SDCARD/Roms/CPC/
-romExts = .dsk,.sna,.tap,.cdt,.voc,.cpr,.m3u,.zip,.7z
+consoleList = APPS,ARDUINO,ATARI 2600,ATARI 5200,ATARI 7800,DAPHNE,FDS,FINALBURN NEO,GAME & WATCH,GAME BOY, GAME BOY ADVANCE,GAME BOY COLOR,GAME GEAR,GAMES,INTELLIVISION,MAME,MASTER SYSTEM,MSU-1,MSU-MD,NEO GEO CD,NEO GEO POCKET,NEO GEO POCKET COLOR,NES,NINTENDO DS,ODYSSEY2,PC ENGINE,PC ENGINE CD,PC-FX,PLAYSTATION,POKEMON MINI,SATELLAVIEW,SEGA 32X,SEGA CD,SEGA GENESIS,SEGA SG-1000,SGB,SNES,SUPERVISION,SUFAMI TURBO,VIRTUAL BOY,WONDERSWAN,WONDERSWAN COLOR
 
 [APPS]
 execs = #
@@ -22,117 +11,67 @@ romExts = .sh,.fgl
 
 [ARDUINO]
 execs = /mnt/SDCARD/.simplemenu/launchers/arduous_libretro
-romDirs = /mnt/SDCARD/Roms/ARDUBOY/
+romDirs = /mnt/SDCARD/Roms/arduboy/
 romExts = .hex
 
 [ATARI 2600]
 execs = /mnt/SDCARD/.simplemenu/launchers/stella2014_libretro
-romDirs = /mnt/SDCARD/Roms/ATARI/
+romDirs = /mnt/SDCARD/Roms/atari2600/
 romExts = .bin,.a26,.zip,.7z
 
 [ATARI 5200]
-execs = /mnt/SDCARD/.simplemenu/launchers/a5200_libretro,/mnt/SDCARD/.simplemenu/launchers/atari800_libretro
-romDirs = /mnt/SDCARD/Roms/FIFTYTWOHUNDRED/
+execs = /mnt/SDCARD/.simplemenu/launchers/atari800_libretro
+romDirs = /mnt/SDCARD/Roms/atari5200/
 romExts = .bin,.a52,.zip,.7z
 
 [ATARI 7800]
 execs = /mnt/SDCARD/.simplemenu/launchers/prosystem_libretro
-romDirs = /mnt/SDCARD/Roms/SEVENTYEIGHTHUNDRED/
+romDirs = /mnt/SDCARD/Roms/atari7800/
 romExts = .bin,.a78,.zip,.7z
-
-[ATARI LYNX]
-execs = /mnt/SDCARD/.simplemenu/launchers/handy_libretro,/mnt/SDCARD/.simplemenu/launchers/mednafen_lynx_libretro
-romDirs = /mnt/SDCARD/Roms/LYNX/
-romExts = .zip,.lnx,.7z
-scaling = 1
-
-[ATARI ST]
-execs = /mnt/SDCARD/.simplemenu/launchers/hatari_libretro,/mnt/SDCARD/.simplemenu/launchers/hatari_plus_libretro
-romDirs = /mnt/SDCARD/Roms/ATARIST/
-romExts = .a78,.rom,.zip,.7z
-
-[COMMODORE 64]
-execs = /mnt/SDCARD/.simplemenu/launchers/vice_x64_libretro
-romDirs = /mnt/SDCARD/Roms/C64/
-romExts = .crt,.d64,.t64,.bin,.g64,.7z,.zip
-
-[CPS1]
-execs = /mnt/SDCARD/.simplemenu/launchers/fbalpha2012_cps1_libretro,/mnt/SDCARD/.simplemenu/launchers/fbneo_libretro,/mnt/SDCARD/.simplemenu/launchers/fbalpha_libretro,/mnt/SDCARD/.simplemenu/launchers/fbneo_miyoo_libretro
-romDirs = /mnt/SDCARD/Roms/CPS1/
-romExts = .zip
-aliasFile = /mnt/SDCARD/.simplemenu/alias.txt
-
-[CPS2]
-execs = /mnt/SDCARD/.simplemenu/launchers/fbalpha2012_cps2_libretro,/mnt/SDCARD/.simplemenu/launchers/fbneo_libretro,/mnt/SDCARD/.simplemenu/launchers/fbalpha_libretro,/mnt/SDCARD/.simplemenu/launchers/fbneo_miyoo_libretro
-romDirs = /mnt/SDCARD/Roms/CPS2/
-romExts = .zip
-aliasFile = /mnt/SDCARD/.simplemenu/alias.txt
-
-[CPS3]
-execs = /mnt/SDCARD/.simplemenu/launchers/fbalpha2012_cps3_libretro,/mnt/SDCARD/.simplemenu/launchers/fbneo_libretro,/mnt/SDCARD/.simplemenu/launchers/fbalpha_libretro,/mnt/SDCARD/.simplemenu/launchers/fbneo_miyoo_libretro
-romDirs = /mnt/SDCARD/Roms/CPS3/
-romExts = .zip
-aliasFile = /mnt/SDCARD/.simplemenu/alias.txt
 
 [DAPHNE]
 execs = /mnt/SDCARD/.simplemenu/launchers/daphne_libretro
-romDirs = /mnt/SDCARD/Roms/DAPHNE/
+romDirs = /mnt/SDCARD/Roms/daphne/º
 romExts = .zip
-
-[DOS]
-execs = /mnt/SDCARD/.simplemenu/launchers/dosbox_pure_libretro,/mnt/SDCARD/.simplemenu/launchers/dosbox_pure_miyoo_libretro
-romDirs = /mnt/SDCARD/Roms/DOS/
-romExts = .zip,.dosz,.exe,.com,.bat,.iso,.cue,.vhd,.m3u,.7z
-
-[DOOM]
-execs = /mnt/SDCARD/.simplemenu/launchers/prboom_libretro
-romDirs = /mnt/SDCARD/Roms/DOOM/DOOM/,/mnt/SDCARD/Roms/DOOM/DOOM2/
-romExts = .wad,.zip,.7z
 
 [FDS]
-execs = /mnt/SDCARD/.simplemenu/launchers/fceumm_libretro,/mnt/SDCARD/.simplemenu/launchers/nestopia_libretro,/mnt/SDCARD/.simplemenu/launchers/nestopia_miyoo_libretro
-romDirs = /mnt/SDCARD/Roms/FDS/
+execs = /mnt/SDCARD/.simplemenu/launchers/fceumm_libretro
+romDirs = /mnt/SDCARD/Roms/fds/
 romExts = .fds,.zip,.7z
 
-[FINALBURN ALPHA]
-execs = /mnt/SDCARD/.simplemenu/launchers/fbalpha2012_libretro,/mnt/SDCARD/.simplemenu/launchers/fbalpha_libretro,/mnt/SDCARD/.simplemenu/launchers/fbneo_libretro,/mnt/SDCARD/.simplemenu/launchers/fbneo_miyoo_libretro,/mnt/SDCARD/.simplemenu/launchers/fb_32b_libretro
-romDirs = /mnt/SDCARD/Roms/FBA/
-romExts = .zip
-aliasFile = /mnt/SDCARD/.simplemenu/alias.txt
-
 [FINALBURN NEO]
-execs = /mnt/SDCARD/.simplemenu/launchers/fbneo_libretro,/mnt/SDCARD/.simplemenu/launchers/fb_32b_libretro,/mnt/SDCARD/.simplemenu/launchers/fbalpha_libretro,/mnt/SDCARD/.simplemenu/launchers/fbneo_miyoo_libretro
-romDirs = /mnt/SDCARD/Roms/FBN/
+execs = /mnt/SDCARD/.simplemenu/launchers/fbneo_libretro,/mnt/SDCARD/.simplemenu/launchers/fb_32b_libretro,/mnt/SDCARD/.simplemenu/launchers/fbneo_miyoo_libretro
+romDirs = /mnt/SDCARD/Roms/fbneo/
 romExts = .zip
 aliasFile = /mnt/SDCARD/.simplemenu/alias.txt
 
 [GAME & WATCH]
-execs = /mnt/SDCARD/.simplemenu/launchers/gw_libretro,/mnt/SDCARD/.simplemenu/launchers/gw_miyoo_libretro
-romDirs = /mnt/SDCARD/Roms/GW/
+execs = /mnt/SDCARD/.simplemenu/launchers/gw_miyoo_libretro
+romDirs = /mnt/SDCARD/Roms/gameandwatch/
 romExts = .mgw,.zip,.7z
 scaling = 1
 
 [GAME BOY]
-execs = /mnt/SDCARD/.simplemenu/launchers/gambatte_libretro,/mnt/SDCARD/.simplemenu/launchers/gearboy_libretro,/mnt/SDCARD/.simplemenu/launchers/sameboy_libretro,/mnt/SDCARD/.simplemenu/launchers/tgbdual_libretro,/mnt/SDCARD/.simplemenu/launchers/tgbdual_libretro_server,/mnt/SDCARD/.simplemenu/launchers/tgbdual_libretro_client,/mnt/SDCARD/.simplemenu/launchers/DoubleCherryGB_libretro
-romDirs = /mnt/SDCARD/Roms/GB/
+execs = /mnt/SDCARD/.simplemenu/launchers/gambatte_libretro
+romDirs = /mnt/SDCARD/Roms/gb/
 romExts = .gb,.gz,.zip,.7z
 scaling = 1
 
-[GAME BOY COLOR]
-execs = /mnt/SDCARD/.simplemenu/launchers/gambatte_libretro,/mnt/SDCARD/.simplemenu/launchers/gearboy_libretro,/mnt/SDCARD/.simplemenu/launchers/sameboy_libretro,/mnt/SDCARD/.simplemenu/launchers/tgbdual_libretro,/mnt/SDCARD/.simplemenu/launchers/tgbdual_libretro_server,/mnt/SDCARD/.simplemenu/launchers/tgbdual_libretro_client,/mnt/SDCARD/.simplemenu/launchers/DoubleCherryGB_libretro
-romDirs = /mnt/SDCARD/Roms/GBC/
-romExts = .gbc,.zip,.7z
-scaling = 1
-
 [GAME BOY ADVANCE]
-execs = /mnt/SDCARD/.simplemenu/launchers/gpsp_plus_libretro,/mnt/SDCARD/.simplemenu/launchers/mgba_plus_libretro,/mnt/SDCARD/.simplemenu/launchers/gpsp_libretro,/mnt/SDCARD/.simplemenu/launchers/mgba_libretro,/mnt/SDCARD/.simplemenu/launchers/mednafen_gba_libretro,/mnt/SDCARD/.simplemenu/launchers/meteor_libretro,/mnt/SDCARD/.simplemenu/launchers/vbam_libretro,/mnt/SDCARD/.simplemenu/launchers/vba_next_libretro
-romDirs = /mnt/SDCARD/Roms/GBA/
+execs = /mnt/SDCARD/.simplemenu/launchers/mgba_libretro
+romDirs = /mnt/SDCARD/Roms/gba/
 romExts = .gba,.zip,.7z
 scaling = 1
 
+[GAME BOY COLOR]
+execs = /mnt/SDCARD/.simplemenu/launchers/gambatte_libretro
+romDirs = /mnt/SDCARD/Roms/gbc/
+romExts = .gbc,.zip,.7z
+scaling = 1
+
 [GAME GEAR]
-execs = /mnt/SDCARD/.simplemenu/launchers/picodrive_libretro,/mnt/SDCARD/.simplemenu/launchers/genesis_plus_gx_libretro,/mnt/SDCARD/.simplemenu/launchers/gearsystem_libretro,/mnt/SDCARD/.simplemenu/launchers/picodrive_standalone
-romDirs = /mnt/SDCARD/Roms/GG/
+execs = /mnt/SDCARD/.simplemenu/launchers/genesis_plus_gx_libretro
+romDirs = /mnt/SDCARD/Roms/gamegear/
 romExts = .zip,.gg,.7z
 scaling = 1
 
@@ -143,186 +82,151 @@ romExts = .sh,.fgl
 
 [INTELLIVISION]
 execs = /mnt/SDCARD/.simplemenu/launchers/freeintv_libretro
-romDirs = /mnt/SDCARD/Roms/INTELLI/
+romDirs = /mnt/SDCARD/Roms/intellivision/
 romExts = .int,.bin
 
 [MAME]
-execs = /mnt/SDCARD/.simplemenu/launchers/mame2003_plus_libretro,/mnt/SDCARD/.simplemenu/launchers/fbneo_libretro,/mnt/SDCARD/.simplemenu/launchers/fbalpha2012_libretro,/mnt/SDCARD/.simplemenu/launchers/km_mame2003_xtreme_amped_libretro,/mnt/SDCARD/.simplemenu/launchers/km_mame2003_xtreme_libretro,/mnt/SDCARD/.simplemenu/launchers/mame2000_libretro,/mnt/SDCARD/.simplemenu/launchers/mame2010_libretro,/mnt/SDCARD/.simplemenu/launchers/mame2003_libretro,/mnt/SDCARD/.simplemenu/launchers/mame2003_xtreme_libretro
+execs = /mnt/SDCARD/.simplemenu/launchers/mame2010_libretro, /mnt/SDCARD/.simplemenu/launchers/mame2015_libretro
 romDirs = /mnt/SDCARD/Roms/ARCADE/
 romExts = .zip
 aliasFile = /mnt/SDCARD/.simplemenu/alias.txt
 
 [MASTER SYSTEM]
-execs = /mnt/SDCARD/.simplemenu/launchers/picodrive_libretro,/mnt/SDCARD/.simplemenu/launchers/genesis_plus_gx_libretro,/mnt/SDCARD/.simplemenu/launchers/gearsystem_libretro,/mnt/SDCARD/.simplemenu/launchers/picodrive_standalone
-romDirs = /mnt/SDCARD/Roms/MS/
+execs = /mnt/SDCARD/.simplemenu/launchers/genesis_plus_gx_libretro
+romDirs = /mnt/SDCARD/Roms/mastersystem/
 romExts = .zip,.sms,.7z
-
-[NEO GEO]
-execs = /mnt/SDCARD/.simplemenu/launchers/fbalpha2012_neogeo_libretro,/mnt/SDCARD/.simplemenu/launchers/fbneo_libretro,/mnt/SDCARD/.simplemenu/launchers/fb_32b_libretro,/mnt/SDCARD/.simplemenu/launchers/fbalpha2012_neogeo_miyoo_libretro,/mnt/SDCARD/.simplemenu/launchers/fbalpha_libretro,/mnt/SDCARD/.simplemenu/launchers/fbneo_miyoo_libretro
-romDirs = /mnt/SDCARD/Roms/NEOGEO/
-romExts = .zip,.7z
-aliasFile = /mnt/SDCARD/.simplemenu/alias.txt
-
-[NEO GEO CD]
-execs = /mnt/SDCARD/.simplemenu/launchers/neocd_libretro
-romDirs = /mnt/SDCARD/Roms/NEOCD/
-romExts = .zip,.chd
-
-[NEO GEO POCKET]
-execs = /mnt/SDCARD/.simplemenu/launchers/race_libretro,/mnt/SDCARD/.simplemenu/launchers/mednafen_ngp_libretro
-romDirs = /mnt/SDCARD/Roms/NGP/
-romExts = .ngp,.ngc,.ngpc,.npc,.zip,.7z
-scaling = 1
-
-[NINTENDO DS]
-execs = /mnt/SDCARD/.simplemenu/launchers/drastic_standalone
-romDirs = /mnt/SDCARD/Roms/NDS/
-romExts = .nds,.7z,.zip,.rar
-scaling = 1
-
-[NES]
-execs = /mnt/SDCARD/.simplemenu/launchers/fceumm_libretro,/mnt/SDCARD/.simplemenu/launchers/nestopia_libretro,/mnt/SDCARD/.simplemenu/launchers/nestopia_miyoo_libretro
-romDirs = /mnt/SDCARD/Roms/FC/
-romExts = .nes,.zip,.7z
 
 [MSU-1]
 execs = /mnt/SDCARD/.simplemenu/launchers/msu1_libretro
-romDirs = /mnt/SDCARD/Roms/MSU1/
+romDirs = /mnt/SDCARD/Roms/snes-msu1/
 romExts = .smc,.sfc,.zip,.7z
 aliasFile = /mnt/SDCARD/.simplemenu/alias_MSU-1.txt
 
 [MSU-MD]
 execs = /mnt/SDCARD/.simplemenu/launchers/MSU-MD_libretro
-romDirs = /mnt/SDCARD/Roms/MSUMD/
+romDirs = /mnt/SDCARD/Roms/msu-md/
 romExts = .md
 aliasFile = /mnt/SDCARD/.simplemenu/alias_MSU-MD.txt
 
-[MSX]
-execs = /mnt/SDCARD/.simplemenu/launchers/bluemsx_libretro,/mnt/SDCARD/.simplemenu/launchers/fmsx_libretro
-romDirs = /mnt/SDCARD/Roms/MSX/
-romExts = .rom,.ri,.mx1,.mx2,.col,.dsk,.cas,.sg,.sc,.m3u,.zip,.7z
+[NEO GEO CD]
+execs = /mnt/SDCARD/.simplemenu/launchers/neocd_libretro
+romDirs = /mnt/SDCARD/Roms/neogeocd/
+romExts = .zip,.chd,.cue
+
+[NEO GEO POCKET]
+execs = /mnt/SDCARD/.simplemenu/launchers/mednafen_ngp_libretro
+romDirs = /mnt/SDCARD/Roms/ngp/
+romExts = .ngp,.ngc,.npc,.zip,.7z
+scaling = 1
+
+[NEO GEO POCKET COLOR]
+execs = /mnt/SDCARD/.simplemenu/launchers/mednafen_ngp_libretro
+romDirs = /mnt/SDCARD/Roms/ngpc/
+romExts = .ngp,.ngc,.ngpc,.npc,.zip,.7z
+scaling = 1
+
+[NES]
+execs = /mnt/SDCARD/.simplemenu/launchers/fceumm_libretro
+romDirs = /mnt/SDCARD/Roms/nes/
+romExts = .nes,.zip,.7z
+
+[NINTENDO DS]
+execs = /mnt/SDCARD/.simplemenu/launchers/drastic_standalone
+romDirs = /mnt/SDCARD/Roms/nds/
+romExts = .nds,.7z,.zip,.rar
+scaling = 1
 
 [ODYSSEY2]
 execs = /mnt/SDCARD/.simplemenu/launchers/o2em_libretro
-romDirs = /mnt/SDCARD/Roms/ODYSSEY2/
+romDirs = /mnt/SDCARD/Roms/o2em/
 romExts = .zip,.bin
-
-[OPENBOR]
-execs = /mnt/SDCARD/.simplemenu/launchers/openbor_standalone
-romDirs = /mnt/SDCARD/Roms/OPENBOR/
-romExts = .pak
-
-[OVERLAYS]
-execs = #
-romDirs = /mnt/SDCARD/.simplemenu/overlays/
-romExts = .sh,.fgl
-
-[PC98]
-execs = /mnt/SDCARD/.simplemenu/launchers/nekop2_libretro,/mnt/SDCARD/.simplemenu/launchers/np2kai_libretro
-romDirs = /mnt/SDCARD/Roms/PC98/
-romExts = .hdi,.fdi
 
 [PC ENGINE]
 execs = /mnt/SDCARD/.simplemenu/launchers/mednafen_pce_fast_libretro,/mnt/SDCARD/.simplemenu/launchers/mednafen_supergrafx_libretro
-romDirs = /mnt/SDCARD/Roms/PCE/
+romDirs = /mnt/SDCARD/Roms/pcengine/
 romExts = .pce,.tg16,.cue,.zip,.7z
 
 [PC ENGINE CD]
 execs = /mnt/SDCARD/.simplemenu/launchers/mednafen_pce_fast_libretro,/mnt/SDCARD/.simplemenu/launchers/mednafen_supergrafx_libretro
-romDirs = /mnt/SDCARD/Roms/PCECD/
+romDirs = /mnt/SDCARD/Roms/pcenginecd/
 romExts = .pce,.tg16,.cue,.chd,.zip,.7z
 
-[PICO-8]
-execs = /mnt/SDCARD/.simplemenu/launchers/pico_standalone,/mnt/SDCARD/.simplemenu/launchers/fake08_libretro,/mnt/SDCARD/.simplemenu/launchers/retro8_libretro
-romDirs = /mnt/SDCARD/Roms/PICO/
-romExts = .png,.p8,.p8.png
-aliasFile = /mnt/SDCARD/.simplemenu/alias_PICO-8.txt
+[PC-FX]
+execs = /mnt/SDCARD/.simplemenu/launchers/mednafen_pcfx_libretro
+romDirs = /mnt/SDCARD/Roms/pcfx/
+romExts = .cue,.chd,.zip,.7z
 
 [PLAYSTATION]
-execs = /mnt/SDCARD/.simplemenu/launchers/pcsx_standalone,/mnt/SDCARD/.simplemenu/launchers/pcsx_rearmed_miyoo_libretro,/mnt/SDCARD/.simplemenu/launchers/pcsx_rearmed_libretro,/mnt/SDCARD/.simplemenu/launchers/pcsx_rearmed_libretro_old,/mnt/SDCARD/.simplemenu/launchers/pcsx_rearmed_libretro_shaun
-romDirs = /mnt/SDCARD/Roms/PS/
+execs = /mnt/SDCARD/.simplemenu/launchers/pcsx_rearmed_miyoo_libretro,/mnt/SDCARD/.simplemenu/launchers/pcsx_rearmed_libretro
+romDirs = /mnt/SDCARD/Roms/psx/
 romExts = .pbp,.chd,.zip,.cue,.img,.iso,.m3u,.mdf
 
 [POKEMON MINI]
 execs = /mnt/SDCARD/.simplemenu/launchers/pokemini_libretro
-romDirs = /mnt/SDCARD/Roms/POKE/
+romDirs = /mnt/SDCARD/Roms/pokemini/
 romExts = .min,.zip,.7z
 scaling = 1
 
-[QUAKE]
-execs = /mnt/SDCARD/.simplemenu/launchers/tyrquake_libretro
-romDirs = /mnt/SDCARD/Roms/QUAKE/id1/,/mnt/SDCARD/Roms/QUAKE/hipnotic/,/mnt/SDCARD/Roms/QUAKE/rogue/,/mnt/SDCARD/Roms/QUAKE/dopa/
-romExts = .fbl
-
-[SCUMMVM]
-execs = /mnt/SDCARD/.simplemenu/launchers/scummvm_libretro
-romDirs = /mnt/SDCARD/Roms/SCUMMVM/
-romExts = .scummvm
+[SATELLAVIEW]
+execs = /mnt/SDCARD/.simplemenu/launchers/snes9x_libretro
+romDirs = /mnt/SDCARD/Roms/satellaview/
+romExts = .bs,.zip,.7z
 
 [SEGA 32X]
-execs = /mnt/SDCARD/.simplemenu/launchers/picodrive_libretro,/mnt/SDCARD/.simplemenu/launchers/picodrive_standalone
-romDirs = /mnt/SDCARD/Roms/THIRTYTWOX/
+execs = /mnt/SDCARD/.simplemenu/launchers/picodrive_libretro
+romDirs = /mnt/SDCARD/Roms/sega32x/
 romExts = .zip,.32x,.7z
 
 [SEGA CD]
-execs = /mnt/SDCARD/.simplemenu/launchers/picodrive_libretro,/mnt/SDCARD/.simplemenu/launchers/genesis_plus_gx_libretro,/mnt/SDCARD/.simplemenu/launchers/picodrive_standalone
-romDirs = /mnt/SDCARD/Roms/SEGACD/
-romExts = .bin,.chd,.cue
+execs = /mnt/SDCARD/.simplemenu/launchers/genesis_plus_gx_libretro
+romDirs = /mnt/SDCARD/Roms/segacd/
+romExts = .chd,.cue
 
 [SEGA GENESIS]
-execs = /mnt/SDCARD/.simplemenu/launchers/picodrive_libretro,/mnt/SDCARD/.simplemenu/launchers/genesis_plus_gx_libretro,/mnt/SDCARD/.simplemenu/launchers/picodrive_standalone
-romDirs = /mnt/SDCARD/Roms/MD/
+execs = /mnt/SDCARD/.simplemenu/launchers/genesis_plus_gx_libretro
+romDirs = /mnt/SDCARD/Roms/megadrive/
 romExts = .zip,.bin,.smd,.md,.mdx,.gen,.7z
 
 [SEGA SG-1000]
-execs = /mnt/SDCARD/.simplemenu/launchers/gearsystem_libretro,/mnt/SDCARD/.simplemenu/launchers/genesis_plus_gx_libretro,/mnt/SDCARD/.simplemenu/launchers/picodrive_standalone
-romDirs = /mnt/SDCARD/Roms/SEGASGONE/
+execs = /mnt/SDCARD/.simplemenu/launchers/genesis_plus_gx_libretro
+romDirs = /mnt/SDCARD/Roms/sg-1000/
 romExts = .zip,.sg,.7z
 
 [SGB]
-execs = /mnt/SDCARD/.simplemenu/launchers/msgb_libretro,/mnt/SDCARD/.simplemenu/launchers/tgbdual_libretro
-romDirs = /mnt/SDCARD/Roms/SGB/
+execs = /mnt/SDCARD/.simplemenu/launchers/msgb_libretro
+romDirs = /mnt/SDCARD/Roms/sgb/
 romExts = .gb,.gbc,.zip,.7z
 
 [SNES]
-execs = /mnt/SDCARD/.simplemenu/launchers/snes9x2005_plus_libretro,/mnt/SDCARD/.simplemenu/launchers/snes9x2005_libretro,/mnt/SDCARD/.simplemenu/launchers/snes9x2002_libretro,/mnt/SDCARD/.simplemenu/launchers/snes9x2010_libretro,/mnt/SDCARD/.simplemenu/launchers/snes9x_libretro,/mnt/SDCARD/.simplemenu/launchers/snes9x_next_libretro,/mnt/SDCARD/.simplemenu/launchers/mednafen_supafaust_plus_libretro,/mnt/SDCARD/.simplemenu/launchers/mednafen_supafaust_libretro
-romDirs = /mnt/SDCARD/Roms/SFC/
+execs = /mnt/SDCARD/.simplemenu/launchers/snes9x_libretro,/mnt/SDCARD/.simplemenu/launchers/mednafen_supafaust_libretro,/mnt/SDCARD/.simplemenu/launchers/mednafen_supafaust_plus_libretro,/mnt/SDCARD/.simplemenu/launchers/snes9x2005_plus_libretro,/mnt/SDCARD/.simplemenu/launchers/snes9x2005_libretro,/mnt/SDCARD/.simplemenu/launchers/snes9x2002_libretro,/mnt/SDCARD/.simplemenu/launchers/snes9x2010_libretro,/mnt/SDCARD/.simplemenu/launchers/snes9x_next_libretro
+romDirs = /mnt/SDCARD/Roms/snes/
 romExts = .smc,.sfc,.zip,.7z
 
 [SUPERVISION]
 execs = /mnt/SDCARD/.simplemenu/launchers/potator_libretro
-romDirs = /mnt/SDCARD/Roms/SUPERVISION/
+romDirs = /mnt/SDCARD/Roms/supervision/
 romExts = .bin,.sv,.zip,.7z
 scaling = 1
 
-[TIC-80]
-execs = /mnt/SDCARD/.simplemenu/launchers/tic80_libretro
-romDirs = /mnt/SDCARD/Roms/TIC/
-romExts = .tic,.7z
+[SUFAMI TURBO]
+execs = /mnt/SDCARD/.simplemenu/launchers/snes9x_libretro
+romDirs = /mnt/SDCARD/Roms/sufami/
+romExts = .st,.zip,.7z
 
 [VIRTUAL BOY]
 execs = /mnt/SDCARD/.simplemenu/launchers/mednafen_vb_libretro
-romDirs = /mnt/SDCARD/Roms/VB/
+romDirs = /mnt/SDCARD/Roms/virtualboy/
 romExts = .vb,.vboy,.bin,.zip,.7z
-
-[WOLF3D]
-execs = /mnt/SDCARD/.simplemenu/launchers/ecwolf_libretro
-romDirs = /mnt/SDCARD/Roms/WOLF3D/
-romExts = .ecwolf
 
 [WONDERSWAN]
 execs = /mnt/SDCARD/.simplemenu/launchers/mednafen_wswan_libretro
-romDirs = /mnt/SDCARD/Roms/WS/
-romExts = .ws,.wsc,.pc2,.zip,.7z
+romDirs = /mnt/SDCARD/Roms/wswan/
+romExts = .ws,.pc2,.zip,.7z
 scaling = 1
 
-[X68000]
-execs = /mnt/SDCARD/.simplemenu/launchers/px68k_libretro,/mnt/SDCARD/.simplemenu/launchers/px68k_miyoo_libretro
-romDirs = /mnt/SDCARD/Roms/X68000/
-romExts = .dim,.zip,.img,.d88,.88d,.hdm,.dup,.2hd,.xdf,.hdf,.cmd,.m3u,.7z
-
-[ZX SPECTRUM]
-execs = /mnt/SDCARD/.simplemenu/launchers/fuse_libretro
-romDirs = /mnt/SDCARD/Roms/ZXS/
-romExts = .tzx,.tap,.z80,.rzx,.scl,.trd,.dsk,.zip,.7z
+[WONDERSWAN COLOR]
+execs = /mnt/SDCARD/.simplemenu/launchers/mednafen_wswan_libretro
+romDirs = /mnt/SDCARD/Roms/wswanc/
+romExts = .ws,.wsc,.pc2,.zip,.7z
+scaling = 1
 EOF
--- base/App/SectionsSelector/scripts/default/scripts/handhelds.sh
+++ base/App/SectionsSelector/scripts/default/scripts/handhelds.sh
@@ -2,76 +2,88 @@ consoles="/mnt/SDCARD/.simplemenu/section_groups/handhelds.ini"
 
 cat > ${consoles} <<EOF
 [CONSOLES]
-consoleList = ARDUINO,GAME BOY,GAME BOY COLOR,GAME BOY ADVANCE,NINTENDO DS,GAME & WATCH,GAME GEAR,ATARI LYNX,NEO GEO POCKET,WONDERSWAN,POKEMON MINI,SUPERVISION
+consoleList = ARDUINO,GAME BOY,GAME BOY COLOR,GAME BOY ADVANCE,NINTENDO DS,GAME & WATCH,GAME GEAR,ATARI LYNX,NEO GEO POCKET,NEO GEO POCKET COLOR,WONDERSWAN,WONDERSWAN COLOR,POKEMON MINI,SUPERVISION
 
 [ARDUINO]
 execs = /mnt/SDCARD/.simplemenu/launchers/arduous_libretro
-romDirs = /mnt/SDCARD/Roms/ARDUBOY/
+romDirs = /mnt/SDCARD/Roms/arduboy/
 romExts = .hex
 
 [GAME BOY]
-execs = /mnt/SDCARD/.simplemenu/launchers/gambatte_libretro,/mnt/SDCARD/.simplemenu/launchers/gearboy_libretro,/mnt/SDCARD/.simplemenu/launchers/sameboy_libretro,/mnt/SDCARD/.simplemenu/launchers/tgbdual_libretro,/mnt/SDCARD/.simplemenu/launchers/tgbdual_libretro_server,/mnt/SDCARD/.simplemenu/launchers/tgbdual_libretro_client,/mnt/SDCARD/.simplemenu/launchers/DoubleCherryGB_libretro
-romDirs = /mnt/SDCARD/Roms/GB/
+execs = /mnt/SDCARD/.simplemenu/launchers/gambatte_libretro
+romDirs = /mnt/SDCARD/Roms/gb/
 romExts = .gb,.gz,.zip,.7z
 scaling = 1
 
 [GAME BOY COLOR]
-execs = /mnt/SDCARD/.simplemenu/launchers/gambatte_libretro,/mnt/SDCARD/.simplemenu/launchers/gearboy_libretro,/mnt/SDCARD/.simplemenu/launchers/sameboy_libretro,/mnt/SDCARD/.simplemenu/launchers/tgbdual_libretro,/mnt/SDCARD/.simplemenu/launchers/tgbdual_libretro_server,/mnt/SDCARD/.simplemenu/launchers/tgbdual_libretro_client,/mnt/SDCARD/.simplemenu/launchers/DoubleCherryGB_libretro
-romDirs = /mnt/SDCARD/Roms/GBC/
+execs = /mnt/SDCARD/.simplemenu/launchers/gambatte_libretro
+romDirs = /mnt/SDCARD/Roms/gbc/
 romExts = .gbc,.zip,.7z
 scaling = 1
 
 [GAME BOY ADVANCE]
-execs = /mnt/SDCARD/.simplemenu/launchers/gpsp_plus_libretro,/mnt/SDCARD/.simplemenu/launchers/mgba_plus_libretro,/mnt/SDCARD/.simplemenu/launchers/gpsp_libretro,/mnt/SDCARD/.simplemenu/launchers/mgba_libretro,/mnt/SDCARD/.simplemenu/launchers/mednafen_gba_libretro,/mnt/SDCARD/.simplemenu/launchers/meteor_libretro,/mnt/SDCARD/.simplemenu/launchers/vbam_libretro,/mnt/SDCARD/.simplemenu/launchers/vba_next_libretro
-romDirs = /mnt/SDCARD/Roms/GBA/
+execs = /mnt/SDCARD/.simplemenu/launchers/mgba_libretro,/mnt/SDCARD/.simplemenu/launchers/mgba_plus_libretro,/mnt/SDCARD/.simplemenu/launchers/gpsp_libretro,/mnt/SDCARD/.simplemenu/launchers/gpsp_plus_libretro
+romDirs = /mnt/SDCARD/Roms/gba/
 romExts = .gba,.zip,.7z
 scaling = 1
 
 [NINTENDO DS]
 execs = /mnt/SDCARD/.simplemenu/launchers/drastic_standalone
-romDirs = /mnt/SDCARD/Roms/NDS/
+romDirs = /mnt/SDCARD/Roms/nds/
 romExts = .nds,.7z,.zip,.rar
 scaling = 1
 
 [GAME & WATCH]
 execs = /mnt/SDCARD/.simplemenu/launchers/gw_libretro,/mnt/SDCARD/.simplemenu/launchers/gw_miyoo_libretro
-romDirs = /mnt/SDCARD/Roms/GW/
+romDirs = /mnt/SDCARD/Roms/gameandwatch/
 romExts = .mgw,.zip,.7z
 scaling = 1
 
 [GAME GEAR]
-execs = /mnt/SDCARD/.simplemenu/launchers/picodrive_libretro,/mnt/SDCARD/.simplemenu/launchers/genesis_plus_gx_libretro,/mnt/SDCARD/.simplemenu/launchers/gearsystem_libretro
-romDirs = /mnt/SDCARD/Roms/GG/
+execs = /mnt/SDCARD/.simplemenu/launchers/picodrive_libretro,/mnt/SDCARD/.simplemenu/launchers/genesis_plus_gx_libretro,/mnt/SDCARD/.simplemenu/launchers/gearsystem_libretro,/mnt/SDCARD/.simplemenu/launchers/picodrive_standalone
+romDirs = /mnt/SDCARD/Roms/gamegear/
 romExts = .zip,.gg,.7z
 scaling = 1
 
 [ATARI LYNX]
 execs = /mnt/SDCARD/.simplemenu/launchers/handy_libretro,/mnt/SDCARD/.simplemenu/launchers/mednafen_lynx_libretro
-romDirs = /mnt/SDCARD/Roms/LYNX/
+romDirs = /mnt/SDCARD/Roms/lynx/
 romExts = .zip,.lnx,.7z
 scaling = 1
 
 [NEO GEO POCKET]
-execs = /mnt/SDCARD/.simplemenu/launchers/race_libretro,/mnt/SDCARD/.simplemenu/launchers/mednafen_ngp_libretro
-romDirs = /mnt/SDCARD/Roms/NGP/
+execs = /mnt/SDCARD/.simplemenu/launchers/mednafen_ngp_libretro, /mnt/SDCARD/.simplemenu/launchers/race_libretro
+romDirs = /mnt/SDCARD/Roms/ngp/
+romExts = .ngp,.ngc,.npc,.zip,.7z
+scaling = 1
+
+[NEO GEO POCKET COLOR]
+execs = /mnt/SDCARD/.simplemenu/launchers/mednafen_ngp_libretro, /mnt/SDCARD/.simplemenu/launchers/race_libretro
+romDirs = /mnt/SDCARD/Roms/ngpc/
 romExts = .ngp,.ngc,.ngpc,.npc,.zip,.7z
 scaling = 1
 
 [WONDERSWAN]
 execs = /mnt/SDCARD/.simplemenu/launchers/mednafen_wswan_libretro
-romDirs = /mnt/SDCARD/Roms/WS/
+romDirs = /mnt/SDCARD/Roms/wswan/
+romExts = .ws,.pc2,.zip,.7z
+scaling = 1
+
+[WONDERSWAN COLOR]
+execs = /mnt/SDCARD/.simplemenu/launchers/mednafen_wswan_libretro
+romDirs = /mnt/SDCARD/Roms/wswanc/
 romExts = .ws,.wsc,.pc2,.zip,.7z
 scaling = 1
 
 [POKEMON MINI]
 execs = /mnt/SDCARD/.simplemenu/launchers/pokemini_libretro
-romDirs = /mnt/SDCARD/Roms/POKE/
+romDirs = /mnt/SDCARD/Roms/pokemini/
 romExts = .min,.zip,.7z
 scaling = 1
 
 [SUPERVISION]
 execs = /mnt/SDCARD/.simplemenu/launchers/potator_libretro
-romDirs = /mnt/SDCARD/Roms/SUPERVISION/
+romDirs = /mnt/SDCARD/Roms/supervision/
 romExts = .bin,.sv,.zip,.7z
 scaling = 1
 EOF
